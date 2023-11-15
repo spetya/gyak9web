224ce81bd922128cfb3a8107de01b5124409f056
Péter Steiner <peter.steiner@stud.uni-corvinus.hu>
2023-11-15 16:53:18 +0100

    final

--- gyak9web/Program.cs
+++ gyak9web/Program.cs
@@ -1,10 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();  //Az API Controllerekhez kell 
+builder.Services.AddEndpointsApiExplorer();   //A Swaggerhez kell
+builder.Services.AddSwaggerGen();             //A Swaggerhez kell
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment()) //A Swagger éles környezetben nem indul
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
+app.UseHttpsRedirection();   //Ha http-vel nyitják az oldalt, átirányít https-re
+
+app.UseDefaultFiles();       //Ha nincs semmi a domain tuán, akkor az index.html-t tölti
+app.MapControllers();        //Az API Controllereket elérhetõvé teszi
+app.UseStaticFiles();        //A wwwroot mappa tartalmát elérhetõvé teszi
 
 app.Run();
